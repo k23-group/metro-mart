9974d6d92f38034a4672c7faf99359b3d8dba160
Kei-K23 <keiksl2301@gmail.com>
2024-07-22 13:43:24 +0630

    Add brand request and controller

--- database/migrations/2024_07_21_144247_create_categories_table.php
+++ database/migrations/2024_07_21_144247_create_categories_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->text('name');
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
             $table->unsignedBigInteger('brand_id');
             $table->foreign('brand_id')->references('id')->on('brands')->cascadeOnDelete();
             $table->timestamps();
